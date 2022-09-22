5ec5a6d1159307ef2b2c8643ba859eeb5795f0c1
Hakan <76572659+hakanakkuss@users.noreply.github.com>
2022-09-22 16:15:36 +0300

    completed quiz one
    created student informations, book informations

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var ogrAd : String = "Hakan"
+var ogrSoyad : String  = "Akkus"
+var ogrNo : Int = 1
+var cinsiyet : String  = "Erkek"
+var dTarihi : String  = "28/03/1995"
+var sinif : String = "Invio Bootcamp"
+var islemNo : Int = 1
+
+var kitapAdi : String  = "Suç ve Ceza"
+var kitapNo : Int = 5
+
+var yazarNo : Int = 2
+var yazarAdi : String  = "Fyodor"
+var yazarSoyadi : String = "Dostoyevski"
+var sayfaSayisi : Int = 565
+var isbNo : Int = 27
+var turAdi : String = "Psikolojik Kurgu"
+var turNo = 12
+var puan : Double = 89.7
+
+print("Öğrencinin Adı                       : \(ogrAd)")
+print("Öğrencinin Soyadı                    : \(ogrSoyad)")
+print("Öğrencinin Cinsiyeti                 : \(cinsiyet)")
+print("Öğrencinin Doğum Tarihi              : \(dTarihi)")
+print("Öğrecinin Sınıfı                     : \(sinif)")
+print("İşlem Numarası                       : \(islemNo)")
+print("Kitabın Adı                          : \(kitapAdi)")
+print("Kitap Numarası                       : \(kitapNo)")
+print("Yazar Numarası                       : \(yazarNo)")
+print("Yazarın Adı                          : \(yazarAdi)")
+print("Yazarın Soyadı                       : \(yazarSoyadi)")
+print("Sayfa Sayısı                         : \(sayfaSayisi)")
+print("Uluslararası Standart Kitap Numarası : \(isbNo)")
+print("Kitabın Türü                         : \(turAdi)")
+print("Tür Numarası                         : \(turNo)")
+print("Kitap Puanı                          : \(puan)")
+
+
+
+
+
+
+
+
+
